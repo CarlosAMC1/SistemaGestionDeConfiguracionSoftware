1505b451d23e2734955a9d6c113083c48ec9f2ed
Carlos Alejandro Maldonado Cancapi <67129185+CarlosAMC1@users.noreply.github.com>
2022-10-20 11:38:47 -0500

    Versión Proyecto

--- Models/METODOLOGIA.cs
+++ Models/METODOLOGIA.cs
@@ -4,7 +4,9 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("METODOLOGIA")]
     public partial class METODOLOGIA
@@ -25,5 +27,116 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PROYECTO> PROYECTO { get; set; }
+
+        public List<METODOLOGIA> ListarTodo()
+        {
+            var metodologia = new List<METODOLOGIA>();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    metodologia = db.METODOLOGIA.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            return metodologia;
+        }
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public METODOLOGIA ObtenerMetodologia(int id)
+        {
+            var metodologia = new METODOLOGIA();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    metodologia = db.METODOLOGIA
+                        .Where(x => x.ID_METODOLOGIA == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return metodologia;
+        }
+
+        public void Eliminar()
+        {
+            var metodologia = ObtenerMetodologia(ID_METODOLOGIA);
+            this.ID_METODOLOGIA = metodologia.ID_METODOLOGIA;
+            this.DESCRIPCION = metodologia.DESCRIPCION;
+            this.ESTADO = false;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Habilitar()
+        {
+            var metodologia = ObtenerMetodologia(ID_METODOLOGIA);
+            this.ID_METODOLOGIA = metodologia.ID_METODOLOGIA;
+            this.DESCRIPCION = metodologia.DESCRIPCION;
+            this.ESTADO = true;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
+
+}
 }
--- Models/PROYECTO.cs
+++ Models/PROYECTO.cs
@@ -4,6 +4,7 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
 
     [Table("PROYECTO")]
@@ -39,5 +40,159 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
         public virtual USUARIO USUARIO { get; set; }
 
         public virtual USUARIO USUARIO1 { get; set; }
+        public List<PROYECTO> ListarTodo()
+        {
+            var proyecto = new List<PROYECTO>();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    proyecto = db.PROYECTO.Include("METODOLOGIA")
+                        .Include("USUARIO")
+                        .ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            return proyecto;
+        }
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_PROYECYO > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public PROYECTO ObtenerProyecto(int id)
+        {
+            var proyecto = new PROYECTO();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    proyecto = db.PROYECTO
+                        .Where(x => x.ID_PROYECYO == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return proyecto;
+        }
+
+        public void Eliminar()
+        {
+            var proyecto = ObtenerProyecto(ID_PROYECYO);
+            this.ID_PROYECYO = proyecto.ID_PROYECYO;
+            this.NOMBRE = proyecto.NOMBRE;
+            this.DESCRIPCION = proyecto.DESCRIPCION;
+            this.ID_CLIENTE = proyecto.ID_CLIENTE;
+            this.ID_METODOLOGIA = proyecto.ID_METODOLOGIA;
+            this.ESTADO = proyecto.ESTADO;
+            this.ID_JEFEPROYECTO = proyecto.ID_JEFEPROYECTO;
+            this.FECHA_INICIO = proyecto.FECHA_INICIO;
+            this.FECHA_FIN = proyecto.FECHA_FIN;
+            this.ESTADO = false;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Habilitar()
+        {
+            var proyecto = ObtenerProyecto(ID_PROYECYO);
+            this.ID_PROYECYO = proyecto.ID_PROYECYO;
+            this.NOMBRE = proyecto.NOMBRE;
+            this.DESCRIPCION = proyecto.DESCRIPCION;
+            this.ID_CLIENTE = proyecto.ID_CLIENTE;
+            this.ID_METODOLOGIA = proyecto.ID_METODOLOGIA;
+            this.ESTADO = proyecto.ESTADO;
+            this.ID_JEFEPROYECTO = proyecto.ID_JEFEPROYECTO;
+            this.FECHA_INICIO = proyecto.FECHA_INICIO;
+            this.FECHA_FIN = proyecto.FECHA_FIN;
+            this.ESTADO = true;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Revision()
+        {
+            var proyecto = ObtenerProyecto(ID_PROYECYO);
+            this.ID_PROYECYO = proyecto.ID_PROYECYO;
+            this.NOMBRE = proyecto.NOMBRE;
+            this.DESCRIPCION = proyecto.DESCRIPCION;
+            this.ID_CLIENTE = proyecto.ID_CLIENTE;
+            this.ID_METODOLOGIA = proyecto.ID_METODOLOGIA;
+            this.ESTADO = proyecto.ESTADO;
+            this.ID_JEFEPROYECTO = proyecto.ID_JEFEPROYECTO;
+            this.FECHA_INICIO = proyecto.FECHA_INICIO;
+            this.FECHA_FIN = proyecto.FECHA_FIN;
+            this.ESTADO = null;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_METODOLOGIA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
 }
