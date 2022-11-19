d6863e1b26686da739067efed98c747c15ea4b7d
Alfredo Huillca <alfhuillcaa@upt.pe>
2022-11-19 15:27:07 -0500

    formulario Entegable

---
+++ Controllers/EntregableController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaGestionDeConfiguracionSoftware.Models;
+using static SistemaGestionDeConfiguracionSoftware.Filtros.AdminFilters;
+namespace SistemaGestionDeConfiguracionSoftware.Controllers
+
+namespace SistemaGestionDeConfiguracionSoftware.Controllers
+{
+    public class EntregableController : Controller
+    {
+        ENTREGABLE clsEntregable = new ENTREGABLE();
+        ETAPA clsEtapa = new ETAPA();
+        // GET: Entregable
+        public ActionResult Index()
+        {
+            ViewBag.Entregable = clsEtapa.ListarTodo();
+            return View(clsEntregable.ListarTodo());
+
+        }
+        public ActionResult Guardar(ETAPA etapa)
+        {
+            if (ModelState.IsValid)
+            {
+                etapa.Guardar();
+                return Redirect("~/Entregable/Index");
+            }
+            else
+            {
+                return View("~/Entregable/Index");
+            }
+        }
+
+        public ActionResult EditEnt(int id = 0)
+        {
+            ViewBag.Entregable = clsEtapa.ListarTodo();
+            return View(id == 0 ? new ENTREGABLE() : clsEntregable.ObtenerEntregable(id));
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            clsEntregable.ID_ENTREGABLE = id;
+            clsEntregable.Eliminar();
+            return Redirect("~/Entregable/Index");
+        }
+
+        public ActionResult Habilitar(int id)
+        {
+            clsEntregable.ID_ENTREGABLE = id;
+            clsEntregable.Habilitar();
+            return Redirect("~/Entregable/Index");
+        }
+    }
+} 
--- Models/ENTREGABLE.cs
+++ Models/ENTREGABLE.cs
@@ -6,6 +6,7 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("ENTREGABLE")]
     public partial class ENTREGABLE
@@ -23,6 +24,122 @@ namespace SistemaGestionDeConfiguracionSoftware.Models
 
         public virtual ETAPA ETAPA { get; set; }
 
-      
+        public List<ENTREGABLE> ListarTodo()
+
+        {
+            var etapa = new List<ENTREGABLE>();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    etapa = db.ENTREGABLE.Include("ETAPA").ToList();
+                }
+            }
+
+            catch (Exception e)
+
+            {
+                throw;
+            }
+
+            return etapa;
+        }
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_ENTREGABLE > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+
+            catch (Exception e)
+
+            {
+                throw;
+            }
+        }
+
+        public ENTREGABLE ObtenerEntregable(int id)
+        {
+            var etapa = new ENTREGABLE();
+
+            try
+            {
+                using (var db = new Model1())
+                {
+                    etapa = db.ENTREGABLE
+                        .Where(x => x.ID_ENTREGABLE == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return etapa;
+        }
+
+        public void Eliminar()
+        {
+            var etapa = ObtenerEntregable(ID_ETAPA);
+            this.ID_ENTREGABLE= etapa.ID_ENTREGABLE;
+            this.NOMBRE = etapa.NOMBRE;
+            this.ID_ETAPA = etapa.ID_ETAPA;
+            this.ESTADO = false;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_ETAPA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Habilitar()
+        {
+            var etapa = ObtenerEntregable(ID_ETAPA);
+            this.ID_ETAPA = etapa.ID_ETAPA;
+            this.NOMBRE = etapa.NOMBRE;
+            this.ID_ETAPA = etapa.ID_ETAPA;
+            this.ESTADO = true;
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.ID_ETAPA > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+
     }
 }
